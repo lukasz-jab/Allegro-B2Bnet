6bd8301a12f678cb6a9332e4b8785da6e99ad4ac
lukasz-jab <lukasz.j.pl@interia.pl>
2020-09-16 19:50:24 +0200

    Corrected ApplicationManager declaration

--- src/test/java/allegro/tests/TestBase.java
+++ src/test/java/allegro/tests/TestBase.java
@@ -7,7 +7,7 @@ import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeSuite
     public void setUp() {
