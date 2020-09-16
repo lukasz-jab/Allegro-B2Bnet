db04e56f072e23c80a892f79d927f6d8a33aa06f
lukasz-jab <lukasz.j.pl@interia.pl>
2020-09-16 20:42:12 +0200

    Added Test due to modification Allegro side panel, after search

--- src/test/java/allegro/tests/AllegroIphoneTest.java
+++ src/test/java/allegro/tests/AllegroIphoneTest.java
@@ -8,12 +8,33 @@ import java.util.List;
 
 public class AllegroIphoneTest extends TestBase {
 
-    @Test
+    @Test(enabled = false)
+    // 16.09.2020 Allegro usunęło panel boczny z kolorem po wyszukiwaniu
     public void testAllegroIphone() {
 
         app.article().fillSearchBar("Iphone 11");
         app.article().submitSearch();
         app.article().selectColor("czarny");
+
+        List<ArticleData> articlesFirstSite = app.article().list();
+
+        System.out.println("Ilość czarnych Iphonów11 na pierwszej stronie wynosi: " + articlesFirstSite.size());
+
+        ArticleData maxPrice =
+                articlesFirstSite.stream().max((o1, o2) -> o1.priceToCalc().compareTo(o2.priceToCalc())).get();
+        System.out.println("Największa cena na liście to: " + maxPrice.priceToCalc() + "zł");
+
+        System.out.println("Największa cena na liście + 23% wynosi: " + (maxPrice.priceToCalc())
+                .multiply(new BigDecimal("1.23")).setScale(2) + "zł");
+    }
+
+    @Test(enabled = true)
+    // 16.09.2020 Test dodany z powodu usunięcia przez Allegro panelu bocznego z kolorem po wyszukiwaniu
+    public void testAllegroIphoneBlack() {
+
+        app.article().fillSearchBar("Iphone 11 kolor czarny");
+        app.article().submitSearch();
+        //app.article().selectColor("czarny");
 
         List<ArticleData> articlesFirstSite = app.article().list();
 
